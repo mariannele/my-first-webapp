f25c8c12fd2890a61fa5a1096ca7424be2261b23
Marianne Lehtonen <marianne.lehtonen@tuni.fi>
2019-03-13 17:35:28 +0200

    Models fixed and developed further

--- applikaatio/admin.py
+++ applikaatio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Businesstrip
+from .models import Businesstrip, Businesspartner
 
 admin.site.register(Businesstrip)
+admin.site.register(Businesspartner)
 
--- applikaatio/models.py
+++ applikaatio/models.py
@@ -1,21 +1,41 @@
-import datetime
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Businesstrip(models.Model):
+    travel_date = models.DateField(default=timezone.now)
     departure = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
-    distance_km = models.IntegerField()
-    trip_date = models.DateTimeField('date published')
-    def __str__(self):
-        tripinfo = self.departure + "-" + self.destination
-        return tripinfo
 
-#class Choice(models.Model):
- #   question = models.ForeignKey(Question, on_delete=models.CASCADE)
-  #  choice_text = models.CharField(max_length=200)
-   # votes = models.IntegerField(default=0)
-    #def __str__(self):
-     #   return self.choice_text
+    CUSTOMER_MEETING = 'CM'
+    SUPPLIER_MEETING = 'SM'
+    OTHER_MEETING = 'OM'
+    INTERNAL_MEETING = 'IM'
+    OFFICE_DAY = 'OD'
+    TRADE_FAIR = 'TF'    
+    REASON_CHOICES = (
+        (CUSTOMER_MEETING, 'Customer meeting'),
+        (SUPPLIER_MEETING, 'Supplier meeting'),
+        (OTHER_MEETING, 'Other meeting'),
+        (INTERNAL_MEETING, 'Internal meeting'),
+        (OFFICE_DAY, 'Office day'),
+        (TRADE_FAIR, 'Trade fair'),
+    )
+    reason = models.CharField(max_length=2, choices=REASON_CHOICES,
+    default=CUSTOMER_MEETING)
+      
+    company = models.ManyToManyField("Businesspartner", blank = True)
+    total_km = models.IntegerField()
+    km_allowance_eur = models.FloatField(blank=True, null=True)
+    notes = models.TextField()
+    compensation_applied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.travel_date} / {self.destination} ({self.reason})'
+
+
+class Businesspartner(models.Model):
+    company_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.company_name
